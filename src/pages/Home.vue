ee36dd41d890e8368583ba9879479e411501804e
AmjadRasheed7 <1211307882@student.mmu.edu.my>
2024-07-29 16:35:18 +0800

    added cards

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,19 +1,37 @@
 <template>
+  <div>
     <NavBar></NavBar>
+  </div>
+  <div class="pa-4">
+    <v-container>
+      <v-row no-gutters>
+        <v-col
+          v-for="i in 10"
+          :key="i"
+          class="pa-6"
+          sm="7"
+          xs="12"
+          md="6"
+          lg="4"
+        >
+          <Card></Card>
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
 </template>
 
 <script>
-
-import NavBar from '../components/NavBar.vue'
+import NavBar from "../components/NavBar.vue";
+import Card from "../components/Card.vue";
 
 export default {
-    components: {
-        NavBar
-    }
-}
-
+  components: {
+    NavBar,
+    Card,
+  },
+};
 </script>
 
 <style>
-
 </style>
