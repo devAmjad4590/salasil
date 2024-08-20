f13f2cf4e41e93c9adeb3ce81588171d7fb87a72
Nasr <1221301196@student.mmu.edu.my>
2024-08-20 22:39:27 +0800

    pulling

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,16 @@
 <template>
   <v-app>
-      <v-main>
-          <router-view></router-view>
-      </v-main>
+    <v-main>
+      <router-view></router-view>
+    </v-main>
   </v-app>
 </template>
 
-
 <script>
-export default{
-  name: 'App',
+export default {
+  name: "A",
   data: () => ({
     //
   }),
-}
-</script>  
-
- 
+};
+</script>
